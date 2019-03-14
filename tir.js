79d99b6518223121b689239e89511cc2d825917c
andytoma <43856136+andytoma@users.noreply.github.com>
2019-03-14 12:19:28 +0300

    Add files via upload

--- tir.js
+++ tir.js
@@ -1,36 +1,54 @@
-var bullets = 10; // cite patroane ai????
+var bullets = 10; // cite patroane
+var quack = new Audio();
+    quack.src = 'Duckquack.mp3';
+var gunshot = new Audio();
+    gunshot.src = 'Gunshot.mp3';
+var bullets_div = document.getElementById('bullets');
+var bullets = 10;
+    bullets_div.innerHTML = bullets;
+var ducks_div = document.getElementById('ducks_killed');
+var ducks = 0;
+    ducks_div.innerHTML = ducks;
+
+
+
+
 
 function addDuck(){
-  var lake = document.getElementById('lake');
-  var duck = `<div id="duck" class="duck" onclick="killDuck();"></div>`;
-  lake.innerHTML = duck;
+    var lake = document.getElementById('lake');
+    var duck = `<div id="duck" class="duck" onclick="killDuck(), score()"></div>`;
+    lake.innerHTML = duck;
 }
-function audioQuack(){
-  var aux = document.getElementById('aux');
-  var audio = `<audio
-        controls
-        src="/media/examples/t-rex-roar.mp3" autoplay>
-            <code>audio</code> element.
-    </audio>`
-  aux.innerHTML = audio;
-}
-
 function moveDuck(){
-  var duck = document.getElementById('duck');
-  duck.style.bottom =randomBottom()+ 'px';
+    var duck = document.getElementById('duck');
+        duck.style.bottom = randomBottom() + "px";
 }
-
 function randomBottom(){
-  var bot = Math.floor(Math.random()*200);
-  return bot;
+    var bot = Math.floor(Math.random() * 180);
+    return bot;
+}
+setInterval(moveDuck, 1000);
+function shot(){
+    bullets--;
+    bullets_div.innerHTML = bullets;
+  if(bullets <= 0 ){
+    alert("GAME OVER");
+    location.reload();
+  }
+  gunshot.play();
+}
+function score(){
+  ducks++;
+  ducks_div.innerHTML = ducks;
+  if(ducks >= 10){
+    alert("Congratulations, you finished the game!");
+    location.reload();
+  }
 }
 
-setInterval(moveDuck, 2000);
 function killDuck(){
-  audioQuack();
+    quack.play()
 }
 
-
-////////////////////////   START GAME   ///////////////////
-
+///////////////////START GAME////////////////////
 addDuck();
